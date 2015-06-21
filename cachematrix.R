cdfb119112c974e753c5dfbd621c1a7e37dc3d6e
rajesh.sikdar <rajesh.sikdar@gmail.com>
2015-06-21 16:33:03 +0530

    completed assg 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## This script has two functions makeCacheMatix and Cachesolve
-## makeCacheMatric function creates a special Matrix object which can cache inverse its inverse
-## cacheSolve function computes the inverse of special matrix, if inverse is already calculated it ruturns
-## inverse from cache
+## This script has two functions makeCacheMatrix and Cachesolve
+## makeCacheMatrix function creates a special Matrix object which can cacheits inverse
+## cacheSolve function computes the inverse of special matrix
+## if inverse is already calculated it ruturns inverse from cache
 
-
+## makeCacheMatrix
 ## Creates Special Matrix which can store its inverse
 ## Does the following function
 ## 1.getmat() : gets the matrix
@@ -12,21 +12,27 @@
 ## 4.setinv() : sets the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        
         inv <- NULL
+        
         setmat <- function(y) {
                 x <<- y
                 inv <<- NULL
-        }
+        }    
+        
         getmat <- function() x
+        
         setinv <- function(invmat) inv <<- invmat
+        
         getinv <- function() inv
-        list(setmat = setmat, getmat = getmat,
+        
+        list(setmat = setmat, 
+             getmat = getmat,
              setinv = setinv,
              getinv = getinv)
         
-
 }
-
+##cacheSolve
 ## Computes the inverse of Special Matrix
 ## Does the following function
 ## 1.gets the inverse from cache if its already computed for same matrix
@@ -36,10 +42,12 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
         inv <- x$getinv()
+        
         if(!is.null(inv)) {
                 message("inverse is already cached getting it....")
                 return(inv)
         }
+        
         data <- x$getmat()
         inv <- solve(data, ...)
         message("computing inverse matrix...")
